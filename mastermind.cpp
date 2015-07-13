0decb79d59def1bfb48845e86502fd8014ec8ed5
Bob Andrews <swerdna@github.com>
2015-07-13 09:20:48 +0700

    Skeleton for unit tests, moved command line parsing to it's own compilation unit for testability, fixed command line parsing (couldn't trigger human codebreaker, after I implemented Knuth).

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,78 +1,26 @@
-#include "getopt.h"
 #include "CGame.h"
-#include "MastermindCommonDefines.h"
-#include <iostream>
-
-//----------------------------------------------------------------------------//
-void usage( const char *a_progName)
-{
-    std::cout << "usage: " << a_progName << " [-c code]" << std::endl
-              << " where code is " << NMasterMind::ctSlots << " digits long, and contains only the"
-              << " digits from 1 to " << NMasterMind::ctPegs << std::endl;
-}
-
-//----------------------------------------------------------------------------//
-
-std::pair<bool, std::string> checkOpts( int argc, char **argv )
-{
-    std::string l_code;
-
-    if (1 < argc)
-    {
-        int c = 0;
-        while (-1 != ( c = getopt( argc, argv, "c:" )))
-        {
-            switch (c)
-            {
-                case 'c':
-                    l_code = optarg;
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    bool l_valid = true;
-    if (NMasterMind::ctSlots != l_code.size())
-    {
-        l_valid = false;
-        l_code.clear();
-    }
-    else
-    {
-        for (auto c : l_code)
-        {
-            if (c < '1' || c > ('0' + NMasterMind::ctPegs))
-            {
-                l_valid = false;
-                l_code.clear();
-                break;
-            }
-        }
-    }
-
-    return std::make_pair( l_valid, l_code );
-}
+#include "CmdLineParsing.h"
 
 //----------------------------------------------------------------------------//
 
 int main( int argc, char **argv )
 {
-    auto l_params = checkOpts( argc, argv );
+    auto l_params = NMasterMind::checkOpts( argc, argv );
 
     if (false == l_params.first)
     {
-        usage(argv[0]);
+        NMasterMind::usage(argv[0]);
     }
     else
     {
-        // options: -c <code>   Sets the code, and triggers a machine match
-
+        // Pass the user-indicated code to the game.
+        // The code may be empty.
         NMasterMind::CGame l_game( l_params.second );
 
         l_game.playGame();
     }
+
+    // TODO: we could return the number of points earnt by the codemaster.
 
     return l_params.first ? 0 : -1;
 }
